f21a0d1aa7af0a6b9cb9b78cec38bf3d2c5b51b1
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-28 00:29:50 +0100

    Added base for view and demo

--- src/Prompt.js
+++ src/Prompt.js
@@ -1,17 +1,82 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import PersonIcon from '@material-ui/icons/Person';
+import AddIcon from '@material-ui/icons/Add';
+import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
+
+const Prompt = {};
+// eslint-disable-next-line no-console
+let Update = () => console.log("Prompt is not ready yet");
+
 
 class View extends React.Component {
-    render() {
-        return (
-            <div>
-                a
-            </div>
-        )
-    }
+	componentDidMount() {
+		Update = () => {
+			this.forceUpdate();
+		};
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{
+                    Object.keys(Prompt).map(key => {
+                        let prompt = Prompt[key]
+                        function handleClose() {
+                            Prompt[key].open = false;
+                            Update()
+                        }
+                        return (
+                            <Dialog key={key} onClose={handleClose} aria-labelledby="simple-dialog-title" open={prompt.open}>
+                            <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+                            <List>
+                              {emails.map(email => (
+                                <ListItem button onClick={() => handleListItemClick(email)} key={email}>
+                                  <ListItemAvatar>
+                                    <Avatar className={classes.avatar}>
+                                      <PersonIcon />
+                                    </Avatar>
+                                  </ListItemAvatar>
+                                  <ListItemText primary={email} />
+                                </ListItem>
+                              ))}
+                      
+                              <ListItem button onClick={() => handleListItemClick('addAccount')}>
+                                <ListItemAvatar>
+                                  <Avatar>
+                                    <AddIcon />
+                                  </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="add account" />
+                              </ListItem>
+                            </List>
+                          </Dialog>
+                        )
+                    })
+                }
+			</React.Fragment>
+		);
+	}
 }
 
-function ask() {
-
+function ask(id,callback,title = "Are you sure?", body = "This action cannot be undone.") {
+    Prompt[id] = {
+        callback,
+        title,
+        body,
+        open: true
+    }
+    Update()
 }
 
 export default {
--- src/PromptDemo.js
+++ src/PromptDemo.js
@@ -1,7 +1,25 @@
-export default function PromptDemo() {
-    return (
-        <div>
-            abc
-        </div>
-    )
+import React from 'react'
+import Prompt from './Prompt'
+import { Button, Container, Box } from '@material-ui/core'
+
+export default class PromptDemo extends React.Component {
+    render() {
+
+        function simplePrompt() {
+            
+        }
+
+        return (
+            <div>
+                <Prompt.View />
+                <Container>
+                    <Box m={16}>
+                        <Button variant="contained" color="primary" onClick={simplePrompt}>
+                            Simple prompt
+                        </Button>
+                    </Box>
+                </Container>
+            </div>
+        )
+    }   
 }
